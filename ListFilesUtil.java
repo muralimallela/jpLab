197fead9fb8d114f472c6f90aa67b6df4a0fd0ad
Murali Krishna Mallela <98264698+muralimallela@users.noreply.github.com>
2023-09-04 08:22:30 +0530

    Add files via upload

---
+++ ListFilesUtil.java
@@ -0,0 +1,59 @@
+package jplab;
+
+import java.io.*;
+import java.lang.*;
+import java.io.File;
+
+public class ListFilesUtil {
+	public void listFilesAndFolders(String directoryName) {
+		File directory = new File(directoryName);
+//get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			System.out.println(file.getName());
+		}
+	}
+
+	public void listFiles(String directoryName) {
+		File directory = new File(directoryName);
+//get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file.isFile()) {
+				System.out.println(file.getName());
+			}
+		}
+
+	}
+
+	public void listFolders(String directoryName) {
+		File directory = new File(directoryName);
+//get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file.isDirectory()) {
+				System.out.println(file.getName());
+			}
+		}
+	}
+
+	public void listFilesAndFilesSubDirectories(String directoryName) {
+		File directory = new File(directoryName);
+//get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file.isFile()) {
+				System.out.println(file.getAbsolutePath());
+			} else if (file.isDirectory()) {
+				listFilesAndFilesSubDirectories(file.getAbsolutePath());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ListFilesUtil listFilesUtil = new ListFilesUtil();
+		final String directoryLinuxMac = "/Users/loiane/test";
+		final String directoryWindows = "E://2-1/";
+		listFilesUtil.listFilesAndFilesSubDirectories(directoryWindows);
+	}
+}
