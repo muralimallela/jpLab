a5c502277b5e00a1561d2bd3fd19a5f1159954b3
Murali Krishna Mallela <98264698+muralimallela@users.noreply.github.com>
2023-09-07 07:39:55 +0530

    Add files via upload

---
+++ Dlist.java
@@ -0,0 +1,20 @@
+package jplab;
+
+import java.util.LinkedList;
+
+public class Dlist {
+
+	public static void main(String[] args) {
+		
+		LinkedList <Integer> list = new LinkedList<>();
+		list.add(10);
+		list.add(4);
+		list.add(15);
+		list.add(24);
+		System.out.println("Double Linked List :"+list);
+		list.remove(2);
+		System.out.println("After Deletion: "+list);
+		
+	}
+
+}
